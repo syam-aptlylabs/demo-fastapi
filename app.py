ea1f8e5ddec5f438fc04c8625285a73e947dd757
syam-aptlylabs <syamagopal@aptlylabs.com>
2026-04-13 21:13:56 +0530

    Added CI/CD pipelines

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import redis
 
 app = FastAPI()
 
-origins = ["*"]
-app.add_middleware(
- CORSMiddleware,
- allow_origins=origins,
- allow_credentials=True,
- allow_methods=["*"],
- allow_headers=["*"],
+# Redis connection
+redis_client = redis.Redis(
+    host="redis",
+    port=6379,
+    decode_responses=True
 )
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/api/test")
 async def test():
- return {"message": "Hello World!"}
+
+    redis_client.set("message", "Hello World!")
+
+    value = redis_client.get("message")
+
+    return {
+        "message": value
+    }
